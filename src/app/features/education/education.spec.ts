500b9d7a9d084cac95568de85b9b4f2532c86dbd
Nabin Khanal <nabinkhanal027@gmail.com>
2026-08-23 11:04:53 -0700

    Build responsive education section

--- src/app/features/education/education.spec.ts
+++ src/app/features/education/education.spec.ts
@@ -13,10 +13,37 @@ describe('Education', () => {
 
     fixture = TestBed.createComponent(Education);
     component = fixture.componentInstance;
+
     await fixture.whenStable();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should display the degree level', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.textContent).toContain("Bachelor's");
+  });
+
+
+  it('should display Idaho State University', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.textContent).toContain(
+      'Idaho State University',
+    );
+  });
+
+
+  it('should not display the major', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.textContent).not.toContain(
+      'Computer Information Systems',
+    );
   });
 });
